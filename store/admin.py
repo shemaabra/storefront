b2f1d38442d4831273aa2a66ec0cd82b43c1d752
shemaabra <abrashema9@gmail.com>
2022-12-24 21:08:46 +0300

    71 to be continued

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
-from .models import Collection, Product, Promotion, Customer
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from .models import (
+    Collection,
+    Product,
+    Order,
+    OrderItem,
+    Promotion,
+    Customer,
+)
 
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "product_count"]
+
+    # overide method of queryset
+    @admin.display(ordering="product_count")
+    def product_count(self, collection):
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": collection.id})
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.product_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(product_count=Count("product"))
 
 
 @admin.register(Product)
@@ -13,6 +36,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ["price"]
     ordering = ["title"]
     list_per_page = 10
+    list_select_related = ["collection"]
 
     @admin.display(ordering="inventory")
     # This decorator Function Allow Sorting Mechanism in This automated column
@@ -22,7 +46,6 @@ class ProductAdmin(admin.ModelAdmin):
         return "Ok"
 
 
-# admin.site.register(Promotion)
 @admin.register(Promotion)
 class PromotionAdmin(admin.ModelAdmin):
     pass
@@ -34,3 +57,17 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ["membership"]
     ordering = ["first_name", "last_name"]
     list_per_page = 10
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "placed_at", "payment_status", "customer"]
+    list_per_page = 10
+    list_select_related = ["customer"]
+    ordering = ["placed_at"]
+    list_editable = ["payment_status"]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
