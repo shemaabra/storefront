71cedb2df1be25c0861d24c43a415e372eb97637
shemaabra <abrashema9@gmail.com>
2022-12-29 11:19:26 +0300

    saving object

--- store/views.py
+++ store/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSeriliarizer
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSeriliarizer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSeriliarizer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSeriliarizer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+ 
 
 
 @api_view()
